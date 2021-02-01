4ec6130c2850abe473d0ef14e28cb6153017364c
Vino1016 <37614592+Vino1016@users.noreply.github.com>
2021-02-01 13:06:48 +0800

    111

--- static/matery/js/matery.js
+++ static/matery/js/matery.js
@@ -91,24 +91,6 @@ $(function () {
     };
     fixStyles();
     /*文章内容详情的一些初始化特性*/
-    let articleInit = function () {
-
-        $('#articleContent img').each(function () {
-            let imgPath = $(this).attr('src');
-            $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
-            // 图片添加阴影
-            $(this).addClass("img-shadow img-margin");
-
-        });
-        // progress bar init
-        const progressElement = window.document.querySelector('.progress-bar');
-        if (progressElement) {
-            new ScrollProgress((x, y) => {
-                progressElement.style.width = y * 100 + '%';
-            });
-        }
-    };
-    articleInit();
     $('.modal').modal();
     $('#backTop').click(function () {
         $('body,html').animate({scrollTop: 0}, 600);
@@ -176,44 +158,40 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 
-    function siteTime() {
-        var seconds = 1000;
-        var minutes = seconds * 60;
-        var hours = minutes * 60;
-        var days = hours * 24;
-        var years = days * 365;
-        var today = new Date();
-        var startYear = "2020";
-        var startMonth = "11";
-        var startDate = "20";
-        var startHour = "0";
-        var startMinute = "0";
-        var startSecond = "0";
-        var todayYear = today.getFullYear();
-        var todayMonth = today.getMonth() + 1;
-        var todayDate = today.getDate();
-        var todayHour = today.getHours();
-        var todayMinute = today.getMinutes();
-        var todaySecond = today.getSeconds();
-        var t1 = Date.UTC(startYear, startMonth, startDate, startHour, startMinute, startSecond);
-        var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
-        var diff = t2 - t1;
-        var diffYears = Math.floor(diff / years);
-        var diffDays = Math.floor((diff / days) - diffYears * 365);
-        var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
-        var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) /
-            minutes);
-        var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours -
-            diffMinutes * minutes) / seconds);
-        if (startYear == todayYear) {
-            document.getElementById("year").innerHTML = todayYear;
-            document.getElementById("sitetime").innerHTML = ":）本站已运行 " + diffDays + " 天 " + diffHours +
-                " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
-        } else {
-            document.getElementById("year").innerHTML = startYear + " - " + todayYear;
-            document.getElementById("sitetime").innerHTML = ":）本站已运行 " + diffYears + " 年 " + diffDays +
-                " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
-        }
+
+    Date.prototype.Format = function(fmt)
+    { //author: meizz
+        var o = {
+            "M+" : this.getMonth()+1,                 //月份
+            "d+" : this.getDate(),                    //日
+            "h+" : this.getHours(),                   //小时
+            "m+" : this.getMinutes(),                 //分
+            "s+" : this.getSeconds(),                 //秒
+            "q+" : Math.floor((this.getMonth()+3)/3), //季度
+            "S"  : this.getMilliseconds()             //毫秒
+        };
+        if(/(y+)/.test(fmt))
+            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+        for(var k in o)
+            if(new RegExp("("+ k +")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        return fmt;
     }
-    setInterval(siteTime, 1000);
+    setTimeout(function(){
+        var starTime = '2020-11-20';
+        var endTime = (new Date()).Format("yyyy-MM-dd");
+        function datedifference(sDate1, sDate2) {    //sDate1和sDate2是2006-12-18格式
+            var dateSpan,
+                tempDate,
+                iDays;
+            sDate1 = Date.parse(sDate1);
+            sDate2 = Date.parse(sDate2);
+            dateSpan = sDate2 - sDate1;
+            dateSpan = Math.abs(dateSpan);
+            iDays = Math.floor(dateSpan / (24 * 3600 * 1000));
+            return iDays
+        };
+        var Days = datedifference(starTime,endTime);
+        document.getElementById("runningTime").innerHTML = Days + " 天";
+    },100);
 });
